1c059365ec1cb77c75dec85eeb183fe9643603d2
trista-ever <Q704355529@163.com>
2017-03-23 17:43:28 +0800

    Update ExcelSheetColumnNumber.java

--- ExcelSheetColumnNumber.java
+++ ExcelSheetColumnNumber.java
@@ -1,8 +1,8 @@
 public class Solution {
     public int titleToNumber(String s) {
          int res=0;
-         for(int i=0;i<s.length();i++){
-             res = res * 26 + (s.charAt(i) - 'A' + 1);
+         for(int i=0;i<s.length();i++){ //start from high position
+             res = res * 26 + (s.charAt(i) - 'A' + 1); // the way to change 'A' to number
          }
          return res;
     }
